6dd8283afbb363d70a161d79d1dbf718cb5416b9
KNIGHTMODE64 <guardknight348@gmail.com>
2026-08-22 00:44:23 +0530

    Remove unused import for background

--- app/src/main/java/com/guardofknight/app/MainActivity.kt
+++ app/src/main/java/com/guardofknight/app/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
